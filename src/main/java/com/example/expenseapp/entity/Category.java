7b10ccdb60448626f63857a3270b9064bed56513
Kubo-Tomoya <t.kubo.cingroup2012@gmail.com>
2026-07-03 02:45:33 +0900

    Entityクラス作成

---
+++ src/main/java/com/example/expenseapp/entity/Category.java
@@ -0,0 +1,34 @@
+package com.example.expenseapp.entity;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="categories")
+public class Category {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 50)
+    private String name;
+
+    @Column(name = "display_order", nullable = false)
+    private Integer displayOrder;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}
